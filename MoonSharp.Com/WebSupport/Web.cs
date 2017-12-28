d86fbd31c296a29129cd42ae4a9c141d6af01fd2
zmjack <jack@zmland.com>
2017-12-28 16:13:28 +0800

    1.0.4

---
+++ MoonSharp.Com/WebSupport/Web.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace MoonSharp.Extensions
+{
+    public class Web
+    {
+        public class ConfigOption
+        {
+            public const string URL_ENCODED = "application/x-www-form-urlencoded";
+            public const string FORM_DATA = "multipart/form-data";
+            public const string GET = "get";
+            public const string POST = "post";
+
+            public string Enctype = URL_ENCODED;
+            public string ContentType = null;
+            public string UserAgent = "";
+            public bool NoBody = false;
+            public string Method = "get";
+            public string Encoding = "utf-8";
+            public string ProxyAddress = "";
+            public string ProxyUsername = "";
+            public string ProxyPassword = "";
+
+            public static explicit operator ConfigOption(Dictionary<string, object> dict)
+            {
+                var instance = new ConfigOption();
+                instance.Enctype = Config.Get(dict, "enctype", instance.Enctype);
+                instance.ContentType = Config.Get(dict, "content-type", instance.ContentType);
+                instance.UserAgent = Config.Get(dict, "user-agent", instance.UserAgent);
+                instance.NoBody = Config.Get(dict, "nobody", instance.NoBody);
+                instance.Method = Config.Get(dict, "method", instance.Method);
+                instance.Encoding = Config.Get(dict, "encoding", instance.Encoding);
+                instance.ProxyAddress = Config.Get(dict, "proxy_address", instance.ProxyAddress);
+                instance.ProxyUsername = Config.Get(dict, "proxy_username", instance.ProxyUsername);
+                instance.ProxyPassword = Config.Get(dict, "proxy_password", instance.ProxyPassword);
+
+                return instance;
+            }
+        }
+
+        public static string Go(string url, Dictionary<string, object> updata, ConfigOption config)
+        {
+            try
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+                var request = (HttpWebRequest)WebRequest.Create(new Uri(url));
+                byte[] requestBody;
+
+                if (config.Enctype != "multipart/form-data")
+                {
+                    //Format: application/x-www-form-urlencoded
+                    var postDataList = new List<string>();
+                    foreach (var data in updata)
+                    {
+                        postDataList.Add($"{data.Key}={HttpUtility.UrlEncode(data.Value.ToString())}");
+                    }
+                    requestBody = Encoding.UTF8.GetBytes(string.Join("&", postDataList));
+                }
+                else
+                {
+                    //Format: multipart/form-data
+                    var formData = new HttpFormData();
+                    foreach (var data in updata)
+                    {
+                        formData.AddData(data.Key, data.Value.ToString());
+                    }
+                    requestBody = formData;
+                }
+
+                if (!config.NoBody)
+                    request.ContentLength = requestBody.Length;
+                request.ContentType = config.ContentType;
+                request.UserAgent = config.UserAgent;
+                request.Method = config.Method;
+                request.Timeout = -1;
+                request.UseDefaultCredentials = true;
+                if (!string.IsNullOrEmpty(config.ProxyAddress))
+                {
+                    request.Proxy = new WebProxy
+                    {
+                        Address = new Uri(config.ProxyAddress),
+                        Credentials = new NetworkCredential
+                        {
+                            UserName = config.ProxyUsername,
+                            Password = config.ProxyPassword,
+                        }
+                    };
+                }
+
+                //Invoke
+                if (config.Method == "post" && !config.NoBody)
+                    request.GetRequestStream().Write(requestBody, 0, requestBody.Length);
+
+                using (var response = request.GetResponse())
+                {
+                    var reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(config.Encoding));
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex) { return $"Exception: {ex.Message}"; }
+        }
+
+    }
+}
--- MoonSharp.Com/WebSupport/WebSupport.cs
+++ MoonSharp.Com/WebSupport/WebSupport.cs
@@ -19,71 +19,7 @@ namespace MoonSharp.Extensions
         {
             try
             {
-                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-                var request = (HttpWebRequest)WebRequest.Create(new Uri(url));
-                byte[] requestBody;
-
-                var enctype = Config.Get(config, "enctype", "application/x-www-form-urlencoded");
-                var contentType = Config.Get(config, "content-type", "application/x-www-form-urlencoded");
-                var userAgent = Config.Get(config, "user-agent", "");
-                var nobody = Config.Get(config, "nobody", @default: false);
-                var method = Config.Get(config, "method", "get");
-                var encoding = Encoding.GetEncoding(Config.Get(config, "encoding", "utf-8"));
-                var proxy_address = Config.Get(config, "proxy_address", "");
-                var proxy_username = Config.Get(config, "proxy_username", "");
-                var proxy_password = Config.Get(config, "proxy_password", "");
-
-                if (enctype != "multipart/form-data")
-                {
-                    //Format: application/x-www-form-urlencoded
-                    var postDataList = new List<string>();
-                    foreach (var data in updata)
-                    {
-                        postDataList.Add($"{data.Key}={HttpUtility.UrlEncode(data.Value.ToString())}");
-                    }
-                    requestBody = Encoding.UTF8.GetBytes(string.Join("&", postDataList));
-                }
-                else
-                {
-                    //Format: multipart/form-data
-                    var formData = new HttpFormData();
-                    foreach (var data in updata)
-                    {
-                        formData.AddData(data.Key, data.Value.ToString());
-                    }
-                    requestBody = formData;
-                }
-
-                if (!nobody)
-                    request.ContentLength = requestBody.Length;
-                request.ContentType = contentType;
-                request.UserAgent = userAgent;
-                request.Method = method;
-                request.Timeout = -1;
-                request.UseDefaultCredentials = true;
-                if (!string.IsNullOrEmpty(proxy_address))
-                {
-                    request.Proxy = new WebProxy
-                    {
-                        Address = new Uri(proxy_address),
-                        Credentials = new NetworkCredential
-                        {
-                            UserName = proxy_username,
-                            Password = proxy_password,
-                        }
-                    };
-                }
-
-                //Invoke
-                if (method == "post" && !nobody)
-                    request.GetRequestStream().Write(requestBody, 0, requestBody.Length);
-
-                using (var response = request.GetResponse())
-                {
-                    var reader = new StreamReader(response.GetResponseStream(), encoding);
-                    return reader.ReadToEnd();
-                }
+                return Web.Go(url, updata, (Web.ConfigOption)config);
             }
             catch (Exception ex) { return $"Exception: {ex.Message}"; }
         }
